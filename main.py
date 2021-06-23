ce78553b07c541eed43012e50b58d17cc312394f
chrischeungtree <aaiop45636@gmail.com>
2021-06-23 17:42:33 +0800

    second commit

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
-
-api.add_resource(HelloWorld, "/helloworld")
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run()
