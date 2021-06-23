467b223bf393452b10bbd4a6448e9eb0eb4da25e
chrischeungtree <aaiop45636@gmail.com>
2021-06-23 22:29:28 +0800

    changed task v2

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
-from flask import Flask, request
-import json
-
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-
-tasks = [
-    {
-        'id': 1,
-        'name': "task1",
-        "description": "This is task 1"
-    },
-    {
-        "id": 2,
-        "name": "task2",
-        "description": "This is task 2"
-    },
-    {
-        "id": 3,
-        "name": "task3",
-        "description": "This is task 3"
-    }
-]
-
-tasksJSON = json.dumps(tasks)
 
 
 @app.route('/')
-def home():
-    return "App Works!!!"
+def hello_world():
+    return 'This is my first API call!'
 
-
-@app.route('/api/tasks')
-def tasks():
-    return tasksJSON
-
-@app.route('/api/task', methods=['POST'])
-def create_task():
-    request_data = request.get_json()
-    tasksData = json.loads(tasksJSON)
-    tasksData.append(request_data['task'])
-    return json.dumps(tasksData);
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
 
 if __name__ == '__main__':
     app.run()
